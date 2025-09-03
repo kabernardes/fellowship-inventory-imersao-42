8304e4fd64bcb3c15b31304414998bc30669adf0
Katia Bernardes <katia.bernardes@codeminer42.com>
2025-09-03 09:26:11 -0300

    feat: add initial code

---
+++ src/data/fellowship.ts
@@ -0,0 +1,16 @@
+export interface Hero {
+  id: number;
+  name: string;
+  race: string;
+  ability: string;
+}
+
+export const fellowship: Hero[] = [
+  { id: 1, name: 'Frodo', race: 'Hobbit', ability: 'Ring Bearer' },
+  { id: 2, name: 'Sam', race: 'Hobbit', ability: 'Loyalty and Courage' },
+  { id: 3, name: 'Aragorn', race: 'Human', ability: 'Leadership' },
+  { id: 4, name: 'Legolas', race: 'Elf', ability: 'Archery' },
+  { id: 5, name: 'Gimli', race: 'Dwarf', ability: 'Axe Mastery' },
+  { id: 6, name: 'Gandalf', race: 'Wizard', ability: 'Magic and Wisdom' },
+  { id: 7, name: 'Boromir', race: 'Human', ability: 'Combat Strength' },
+];
